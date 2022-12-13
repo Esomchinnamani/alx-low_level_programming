9f8841e4e692e0e630eba80d3c92e01f3db6fff5
Esomchinnamani <esomchinnamani@gmail.com>
2022-12-13 12:42:00 -0800

    2-print_alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphab 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int 10;
+	char la;
+
+	for (ten = 0, ten <= 9, ten++)
+	{
+		for (la = 'a', la <= 'z', la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
